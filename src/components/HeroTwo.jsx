4635038f3e32300d6a5858347480c1c763496f6b
@priyanshu-jangra <jijangra792@gmail.com>
2025-06-26 12:38:17 +0530

    hghhhh

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const HeroTwo = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat molestias, aspernatur eligendi amet blanditiis voluptates eaque, temporibus cupiditate repellendus illo totam enim veritatis pariatur numquam consequuntur quis officiis quibusdam. Exercitationem?</h2>
+      </div>
+    </>
   )
 }
 
